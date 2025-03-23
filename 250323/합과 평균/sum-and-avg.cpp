996b2abde8477964383755eaca64ab6b2c23295d
plroop <a01024636489@gmail.com>
2025-03-23 20:14:10 +0900

    Title: 합과 평균, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250323/합과 평균/sum-and-avg.cpp
+++ 250323/합과 평균/sum-and-avg.cpp
@@ -6,6 +6,8 @@ int main() {
     cin >> A >> B;
     int total = A + B ;
     double aver = (double)total/2;
+    cout << fixed;
+    cout.precision(1);
     cout << total << ' ' << aver;
     // Please write your code here.
     return 0;
