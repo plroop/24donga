11d1b7e819c363f44e35a778bcc55c0768f36947
plroop <a01024636489@gmail.com>
2025-03-23 20:32:49 +0900

    Title: 합과 평균의 차, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250323/합과 평균의 차/sub-of-average-and-sum.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double a,b,c;
+    cin >> a >> b >> c;
+    double hap = a + b + c ;
+    double aver = (double)hap/3;
+    cout << hap << endl << aver << endl << hap - aver;
+    
+    // Please write your code here.
+    return 0;
+}
