1a02d87cb80eeba26b547462c61d40869784a4ae
plroop <a01024636489@gmail.com>
2025-03-23 23:09:50 +0900

    Title: 문자 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250323/문자 출력하기/print-text.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char N;
+    cin >> N;
+    for(int i = 0; i < 8; i++){
+        cout<<N;}// Please write your code here.
+    return 0;
+}
