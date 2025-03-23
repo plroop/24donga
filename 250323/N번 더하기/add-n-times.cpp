7c683432e053d6090d0d5e1cbfd5705e2d98602b
plroop <a01024636489@gmail.com>
2025-03-23 23:21:22 +0900

    Title: N번 더하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250323/N번 더하기/add-n-times.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   int A,N;
+   cin >> A >> N;
+   for(int i=1; i<7;i++){
+    cout << A+N*i <<endl;
+   }
+   
+   
+   
+    // Please write your code here.
+    return 0;
+}
