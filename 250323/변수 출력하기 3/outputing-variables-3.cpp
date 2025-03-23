ebcef466e14b0e21f7fe0df5b00bd9148ec19e6d
plroop <a01024636489@gmail.com>
2025-03-23 13:20:40 +0900

    Title: 변수 출력하기 3, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250323/변수 출력하기 3/outputing-variables-3.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   int a,b;
+   char c='C';
+   a=1; b=2; 
+   cout<<a<<"->"<<b<<"->"<<c ;// Please write your code here.
+    return 0;
+}
