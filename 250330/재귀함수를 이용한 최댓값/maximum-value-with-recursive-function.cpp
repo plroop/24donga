173490b97a3b8a093df264b6a9cc78926bcc334f
plroop <a01024636489@gmail.com>
2025-03-30 14:44:33 +0900

    Title: 재귀함수를 이용한 최댓값, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250330/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+int arr[100];
+
+
+int findMax(int arr[], int idx) {
+    if (idx == 0) return arr[0]; 
+    return max(arr[idx], findMax(arr, idx - 1)); 
+}
+
+int main() {
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    // 최댓값 출력
+    cout << findMax(arr, n - 1) << endl;
+
+    return 0;
+}
