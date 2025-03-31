0bb2ec33a4f42f16cc40c63642a0658931585ba1
plroop <a01024636489@gmail.com>
2025-03-31 10:58:14 +0900

    Title: 정수 입력받아 배수 출력, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250331/정수 입력받아 배수 출력/print-multiple-of-input.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    for(int i = 1; i<6; i++)
+    cout << N*i<< ' ';// Please write your code here.
+    return 0;
+}
