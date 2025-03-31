c7bdb5a3673f15893fd6cb38bd576eca109f9248
plroop <a01024636489@gmail.com>
2025-03-31 14:18:58 +0900

    Title: 점수 비교, Time: 6ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250331/점수 비교/score-comparison.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   
+int A,B,C,D;
+
+cin >> A >> B ;
+cin >> C >> D ;
+
+  if (A>B and C>D ){
+    cout << 1;
+  }
+  else{
+    cout << 0;
+  }
+   
+   
+   
+   
+    // Please write your code here.
+    return 0;
+}
