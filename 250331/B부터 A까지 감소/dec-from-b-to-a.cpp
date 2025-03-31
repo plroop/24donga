ec51e156bc54b233a1511b03090a68f028f9c756
plroop <a01024636489@gmail.com>
2025-03-31 11:19:54 +0900

    Title: B부터 A까지 감소, Time: 5ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250331/B부터 A까지 감소/dec-from-b-to-a.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b;
+    cin >> a >> b;
+    for(int i = 0;i<b-a+1;i++){
+        cout << b-i<< ' ';
+    }
+    
+    
+    
+    // Please write your code here.
+    return 0;
+}
