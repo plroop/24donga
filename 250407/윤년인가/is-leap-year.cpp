5f514041cbedae426062c7d7a25baa18493a4131
plroop <a01024636489@gmail.com>
2025-04-07 14:19:38 +0900

    Title: 윤년인가, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250407/윤년인가/is-leap-year.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    cin >> a;
+    if(a%4==0){
+        if(a%100==0 and a%400!=0){
+            cout <<  "false";
+        }else{
+            cout << "true";
+        }
+    }else{
+        cout << "false";
+    }
+    // Please write your code here.
+    return 0;
+}
