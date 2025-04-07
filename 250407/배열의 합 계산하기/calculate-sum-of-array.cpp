c05ec1a85a1ceb1e15aa3cad17a1e0eaecaaec31
plroop <a01024636489@gmail.com>
2025-04-07 14:39:54 +0900

    Title: 배열의 합 계산하기, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250407/배열의 합 계산하기/calculate-sum-of-array.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int array[5] = {3,1,4,5,9};
+    cout << array[1]+array[3]+array[4];
+    // Please write your code here.
+    return 0;
+}
