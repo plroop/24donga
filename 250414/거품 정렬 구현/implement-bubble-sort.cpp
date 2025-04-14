4bd55eec0cbf960618dd1940b7783fd61e218991
plroop <a01024636489@gmail.com>
2025-04-14 14:08:00 +0900

    Title: 거품 정렬 구현, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250414/거품 정렬 구현/implement-bubble-sort.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <algorithm>
+
+#define MAX_N 100
+
+using namespace std;
+
+// 변수 선언
+int n;
+int arr[MAX_N];
+
+void BubbleSort() {
+    bool sorted = true;
+
+    do {
+        sorted = true;
+        for(int i = 0; i < n - 1; i++)
+            if(arr[i] > arr[i + 1]) {
+                int temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+                sorted = false;
+            }
+    } while(!sorted);
+}
+
+int main() {
+    // 입력
+    cin >> n;
+
+    for(int i = 0; i < n; i++)
+        cin >> arr[i];
+    
+    BubbleSort();
+
+    for(int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    return 0;
+}
