578e999a96a64f852016e0dffcdae143d740674c
plroop <a01024636489@gmail.com>
2025-04-27 19:53:12 +0900

    Title: 병합 정렬 구현, Time: 40ms, Memory: 1MB, Status: Passed - Codetree

---
+++ 250427/병합 정렬 구현/implement-merge-sort.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+int n;
+int arr[100000];
+
+void merge(int arr[], int left, int mid, int right) {
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+
+    int* L = new int[n1];
+    int* R = new int[n2];
+
+    for (int i = 0; i < n1; i++) L[i] = arr[left + i];
+    for (int j = 0; j < n2; j++) R[j] = arr[mid + 1 + j];
+
+    int i = 0, j = 0, k = left;
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) arr[k++] = L[i++];
+        else arr[k++] = R[j++];
+    }
+
+    while (i < n1) arr[k++] = L[i++];
+    while (j < n2) arr[k++] = R[j++];
+
+    delete[] L;
+    delete[] R;
+}
+
+void merge_sort(int arr[], int left, int right) {
+    if (left < right) {
+        int mid = (left + right) / 2;
+        merge_sort(arr, left, mid);
+        merge_sort(arr, mid + 1, right);
+        merge(arr, left, mid, right);
+    }
+}
+
+int main() {
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    merge_sort(arr, 0, n - 1);
+
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << ' ';
+    }
+
+    return 0;
+}
